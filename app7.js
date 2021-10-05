8ed608722d58e8e82365060bfa128c5adbf4946a
fbrown8 <francesca.brown.cics@ust.edu.ph>
2021-10-05 12:54:03 +0800

    added app7.js

---
+++ app7.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const mysql2 = require('mysql2');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+
+
+const con = mysql2.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "Cheska Brown",
+    password: "sese2!"
+   
+  })
+
+
+con.connect((err) =>{
+    if(!err){
+        console.log("connected to MySQL server at port 3306...");
+    }else{
+        console.log("error not connected to MYSQL Server")
+    }
+});
+
+app.get("/first", (req, res) => {
+    let sql = "CREATE DATABASE GlobalCovidDb";
+    con.query(sql, (err,result) => {
+        if(!err){
+            res.send("Successfully created database");
+        }else{
+            res.send("failed to create GlobalCovidDB database....")
+        }
+    });
+});
+
+
+app.get("/second", (req, res) => {
+    let sql = "CREATE TABLE stat(CountryId int AUTO_INCREMENT, TotalCases int, TotalTests int, TotalDeaths int, PRIMARY KEY(CountryId))";
+    con.query(sql, (err,result) => {
+        if(!err){
+            res.send(result);
+        }else{
+            res.send("failed to create stat table....")
+        }
+    });
+});
+
+
+
+app.listen(PORT, () => {
+    console.log("Listening to PORT " + PORT + "....");
+   });
